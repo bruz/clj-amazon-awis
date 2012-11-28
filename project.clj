d662ee4b8c7c5db66fd1cc7a303be879037464c2
Bruz Marzolf <bruz@bruzilla.com>
2012-11-27 22:38:02 -0800

    Point at non-snapshot version of camel-snake-kebab to make Clojars happy

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "A thin wrapper for the Amazon AWIS API"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.5.8"]
-                 [camel-snake-kebab "0.1.0-SNAPSHOT"]
+                 [camel-snake-kebab "0.1.0"]
                  [ring "1.1.6"]]
   :dev-dependencies [[midje "1.4.0"]
                      [com.stuartsierra/lazytest "1.2.3"]
