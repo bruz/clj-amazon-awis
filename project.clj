f0bf15e601792fa44ee94d3be278acd134132582
Bruz Marzolf <bruz@bruzilla.com>
2012-11-29 05:58:03 -0800

    Oops github link was wrong

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clj-amazon-awis "0.1.1"
+(defproject clj-amazon-awis "0.1.2"
   :description "A thin wrapper for the Amazon AWIS API"
-  :url "https://github.com/bruz/aws-amazon-awis"
+  :url "https://github.com/bruz/clj-amazon-awis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
